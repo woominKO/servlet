f04925ec382d8d6c55044f25c49549b13957b39a
Woomin Ko <woomin422@gmail.com>
2025-09-20 20:49:27 +0900

    ㅇㅇ

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SpringMemberFormControllerV1 {
-
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process() {
         return new ModelAndView("new-form");
